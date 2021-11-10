e6e1514ff5529d64e4e26de2066e4301e4a174c4
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-11-10 17:53:46 +0100

    Add Head and VerticalMenu to _app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import { useEffect } from 'react';
 import { scrollRestoration } from '../initialAppConfig';
+import Head from 'next/head';
+import VerticalMenu from '../containers/VerticalMenu';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -7,7 +10,17 @@ function MyApp({ Component, pageProps }) {
     scrollRestoration();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <VerticalMenu />
+    </>
+  );
 }
 
 export default MyApp;
