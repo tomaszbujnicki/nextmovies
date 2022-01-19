02003de64aaee9fafd61ebb495a2e781f26b102c
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2022-01-19 23:49:29 +0100

    Svg

---
+++ components/Svg.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './styles/Svg.module.css';
+import * as svgImages from '../assets/SvgButtons';
+
+const Svg = ({ size, name, className }) => {
+  return <div></div>;
+};
+
+export default Svg;
